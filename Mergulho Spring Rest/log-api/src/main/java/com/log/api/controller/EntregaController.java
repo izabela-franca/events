7f12d2488378efd1159f574cb12a65fd82e13712
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2023-03-18 08:09:03 -0300

    Add files via upload

--- Mergulho Spring Rest/log-api/src/main/java/com/log/api/controller/EntregaController.java
+++ Mergulho Spring Rest/log-api/src/main/java/com/log/api/controller/EntregaController.java
@@ -14,8 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.log.api.model.DestinatarioModel;
+import com.log.api.assembler.EntregaAssembler;
 import com.log.api.model.EntregaModel;
+import com.log.api.model.input.EntregaInput;
 import com.log.domain.model.Entrega;
 import com.log.domain.repository.EntregaRepository;
 import com.log.domain.service.SolicitacaoEntregaService;
@@ -29,39 +30,26 @@ public class EntregaController {
 	
 	private EntregaRepository entregaRepository;
 	private SolicitacaoEntregaService solicitacaoEntregaService;
+	private EntregaAssembler entregaAssembler;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
-		return solicitacaoEntregaService.solicitar(entrega);
+	public EntregaModel solicitar(@Valid @RequestBody EntregaInput entregaInput) {
+		Entrega novaEntrega = entregaAssembler.toEntity(entregaInput);
+		
+		Entrega entregaSolicitada = solicitacaoEntregaService.solicitar(novaEntrega);
+		return entregaAssembler.toModel(entregaSolicitada);
 	}
 
 	@GetMapping
-	public List<Entrega> listar() {
-		return entregaRepository.findAll();
+	public List<EntregaModel> listar() {
+		return entregaAssembler.toCollectionModel(entregaRepository.findAll());
 	}
 	
 	@GetMapping("/{entregaId}")
 	public ResponseEntity<EntregaModel> buscar (@PathVariable Long entregaId) {
 		return entregaRepository.findById(entregaId)
-				.map(entrega -> {
-					EntregaModel entregaModel = new EntregaModel();	
-					entregaModel.setId(entrega.getId());
-					entregaModel.setNomeCliente(entrega.getCliente().getNome());
-					entregaModel.setDestinatario(new DestinatarioModel());
-					entregaModel.getDestinatario().setNome(entrega.getDestinatario().getNome());
-					entregaModel.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
-					entregaModel.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
-					entregaModel.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
-					entregaModel.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
-					entregaModel.setTaxa(entrega.getTaxa());
-					entregaModel.setStatus(entrega.getStatus());
-					entregaModel.setDataPedido(entrega.getDataPedido());
-					entregaModel.setDataFinalização(entrega.getDataFinalizacao());
-					
-					
-					return ResponseEntity.ok(entregaModel);
-				})
+				.map(entrega -> ResponseEntity.ok(entregaAssembler.toModel(entrega)))
 				.orElse(ResponseEntity.notFound().build());
 	}
 }
---
+++ Mergulho Spring Rest/log-api/src/main/java/com/log/api/model/input/DestinatarioInput.java
@@ -0,0 +1,26 @@
+package com.log.api.model.input;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DestinatarioInput {
+
+	@NotBlank
+	private String nome;
+	
+	@NotBlank
+	private String logradouro;
+	
+	@NotBlank
+	private String numero;
+	
+	private String complemento;
+	
+	@NotBlank
+	private String bairro;
+	
+}
